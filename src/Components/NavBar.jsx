48e07a47f9d768e791adb1ba45fb79827c65153f
Ju Piazzoli <jpdomeneghetti@gmail.com>
2025-05-14 13:33:04 -0300

    Atualização e complemento (NavBar)

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -5,16 +5,16 @@ import { Link } from "react-router-dom"
 function Nav() {
   return (
     <nav className="navegacao">
-      <Link to="/"> Home </Link> 
-      <Link to="/CRISE 1929"> CRISE 1929 </Link>
-      <Link to="/FASCISMO ITALIANO"> FASCISMO ITALIANO </Link>
+      <Link to="/"> HOME </Link> 
+      <Link to="/CRISE_1929"> CRISE 1929 </Link>
+      <Link to="/FASCISMO_ITALIANO"> FASCISMO ITALIANO </Link>
       <Link to="/CANUDOS"> GUERRA DE CANUDOS </Link>
       <Link to="/CONTESTADO"> GUERRA DO CONTESTADO </Link>
-      <Link to="/1º GUERRA MUNDIAL"> 1º GUERRA MUNDIAL </Link>
-      <Link to="/REVOLUÇÃO RUSSA"> REVOLUÇÃO RUSSA </Link>
-      <Link to="/REVOLUÇÃO 1930"> REVOLUÇÃO 1930 </Link>
+      <Link to="/1º_GUERRA_MUNDIAL"> 1º GUERRA MUNDIAL </Link>
+      <Link to="/REVOLUÇÃO_RUSSA"> REVOLUÇÃO RUSSA </Link>
+      <Link to="/REVOLUÇÃO_1930"> REVOLUÇÃO 1930 </Link>
       <Link to="/FORMULÁRIO"> FORMULÁRIO </Link>
-      <Link to="/AS GATITAS "> AS GATITAS </Link>
+      <Link to="/AS_GATITAS "> AS GATITAS </Link>
     </nav>
   );
 }
