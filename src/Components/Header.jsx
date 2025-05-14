5cb85ba82cd498d35e33d3cedc53dff36aee0e87
Ju Piazzoli <jpdomeneghetti@gmail.com>
2025-05-14 12:09:23 -0300

    Header - Complemento

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -4,12 +4,15 @@ import "./Header.css"
 function Header(props) {
   return (
     <>
-    <header>
-    <h2> {props.titulo} </h2> 
-    <img className="Imagem" src={props.imge} alt="Viagem" />
+     <header>
+      <span className="menu-icon">&#9776;</span> {/* Ícone de menu */}
+      <h1>{props.titulo}</h1>
+      <img className="Imagem" src={props.imge} alt="Logo" />
     </header>
     </>
   );
 }
 
 export default Header;
+
+
