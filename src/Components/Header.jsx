c5d45b98e284b501cf55e872455fa939275d93a8
Ju Piazzoli <jpdomeneghetti@gmail.com>
2025-05-14 13:32:47 -0300

    Atualização e complemento (Header)

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,18 +1,14 @@
-import "./Header.css"
-
+import "./Header.css";
 
 function Header(props) {
   return (
-    <>
-     <header>
-      <span className="menu-icon">&#9776;</span> {/* Ícone de menu */}
+    <header className="Flex-row">
+      <span className="menu-icon" onClick={props.onMenuClick}>&#9776;</span>
       <h1>{props.titulo}</h1>
       <img className="Imagem" src={props.imge} alt="Logo" />
     </header>
-    </>
   );
 }
 
 export default Header;
-
 
