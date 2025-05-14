c3ff2e9b9aa676487c52ec9b3519697e0cc18ad3
Ju Piazzoli <jpdomeneghetti@gmail.com>
2025-05-14 15:58:10 -0300

    NavBar

---
+++ src/Components/NavBar(Cinza).jsx
@@ -0,0 +1,23 @@
+import { Link } from "react-router-dom"
+import "./NavBar(Cinza).css"
+
+
+function Nav() {
+  return (
+    <nav className="navegacaoo">
+      <Link to="/"> HOME </Link> 
+      <Link to="/CRISE_1929"> CRISE 1929 </Link>
+      <Link to="/FASCISMO_ITALIANO"> FASCISMO ITALIANO </Link>
+      <Link to="/CANUDOS"> GUERRA DE CANUDOS </Link>
+      <Link to="/CONTESTADO"> GUERRA DO CONTESTADO </Link>
+      <Link to="/1º_GUERRA_MUNDIAL"> 1º GUERRA MUNDIAL </Link>
+      <Link to="/REVOLUÇÃO_RUSSA"> REVOLUÇÃO RUSSA </Link>
+      <Link to="/REVOLUÇÃO_1930"> REVOLUÇÃO 1930 </Link>
+      <Link to="/FORMULÁRIO"> FORMULÁRIO </Link>
+      <Link to="/AS_GATITAS "> AS GATITAS </Link>
+    </nav>
+  );
+}
+
+export default Nav;
+
