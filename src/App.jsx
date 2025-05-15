4a357b47e4712fa69239b8c6858a59335b65194b
Ju Piazzoli <jpdomeneghetti@gmail.com>
2025-05-15 10:57:06 -0300

    APP - Implementação

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import HomePage from "./Pages/HomePage";
 import GuerraDeCanudos from "./Pages/Guerra_De_Canudos";
 import GuerraDoContestado from "./Pages/Guerra_Do_Contestado";
 import PrimeiraGuerraMundial from "./Pages/1ºGuerra_Mundial";
-//import RevolucaoRussa from "./Pages/Revolução_Russa";
+import RevolucaoRussa from "./Pages/Revolução_Russa";
 //import Revolucao1930 from "./Pages/Revolução_1930";
 import Formulario from "./Pages/Formulario";
 //import AsGatitas from "./Pages/AsGatitas";
@@ -20,7 +20,7 @@ import Formulario from "./Pages/Formulario";
         <Route path="/CANUDOS" element={<GuerraDeCanudos />} />
         <Route path="/CONTESTADO" element={<GuerraDoContestado />} />
         <Route path="/1º_GUERRA_MUNDIAL" element={<PrimeiraGuerraMundial />} />
-        {/* <Route path="/REVOLUÇÃO_RUSSA" element={<RevolucaoRussa />} /> */}
+        <Route path="/REVOLUÇÃO_RUSSA" element={<RevolucaoRussa />} />
         {/* <Route path="/REVOLUÇÃO_1930" element={<Revolucao1930 />} /> */}
          <Route path="/FORMULÁRIO" element={<Formulario />} /> 
         {/* <Route path="/AS_GATITAS" element={<AsGatitas />} /> */}
