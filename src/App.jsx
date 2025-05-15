1945deecdee60cc4766c34644491e8f593e62c30
giihdot <giihferreira0708@gmail.com>
2025-05-15 11:13:56 -0300

    app

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./Pages/HomePage";
-<<<<<<< HEAD
 import Crise1929 from "./Pages/Crise_1929";
-//import FascismoItaliano from "./Pages/Fascismo_Italiano";
-import GuerraDeCanudos from "./Pages/Guerra_De_Canudos";
-import GuerraDoContestado from "./Pages/Guerra_Do_Contestado";
-import PrimeiraGuerraMundial from "./Pages/1ºGuerra_Mundial";
-//import RevolucaoRussa from "./Pages/Revolução_Russa";
 import Revolucao1930 from "./Pages/Revolução_1930";
-=======
-//import Crise1929 from "./Pages/Crise_1929";
 import FascismoItaliano from "./Pages/Fascismo_Italiano";
 import GuerraDeCanudos from "./Pages/Guerra_De_Canudos";
 import GuerraDoContestado from "./Pages/Guerra_Do_Contestado";
 import PrimeiraGuerraMundial from "./Pages/1ºGuerra_Mundial";
 import RevolucaoRussa from "./Pages/Revolução_Russa";
-//import Revolucao1930 from "./Pages/Revolução_1930";
->>>>>>> 78e475b453794cb1213734864d6e89c2e0116785
 import Formulario from "./Pages/Formulario";
 //import AsGatitas from "./Pages/AsGatitas";
 
@@ -25,24 +15,20 @@ import Formulario from "./Pages/Formulario";
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
-<<<<<<< HEAD
         <Route path="/CRISE_1929" element={<Crise1929 />} />
         {/* <Route path="/FASCISMO_ITALIANO" element={<FascismoItaliano />} /> */}
         <Route path="/CANUDOS" element={<GuerraDeCanudos />} />
         <Route path="/CONTESTADO" element={<GuerraDoContestado />} />
         <Route path="/1º_GUERRA_MUNDIAL" element={<PrimeiraGuerraMundial />} />
         {/* <Route path="/REVOLUÇÃO_RUSSA" element={<RevolucaoRussa />} /> */}
-        <Route path="/REVOLUÇÃO_1930" element={<Revolucao1930 />} />
-=======
         {/* <Route path="/CRISE_1929" element={<Crise1929 />} /> */}
         <Route path="/FASCISMO_ITALIANO" element={<FascismoItaliano />} />
         <Route path="/CANUDOS" element={<GuerraDeCanudos />} />
         <Route path="/CONTESTADO" element={<GuerraDoContestado />} />
         <Route path="/1º_GUERRA_MUNDIAL" element={<PrimeiraGuerraMundial />} />
         <Route path="/REVOLUÇÃO_RUSSA" element={<RevolucaoRussa />} />
-        {/* <Route path="/REVOLUÇÃO_1930" element={<Revolucao1930 />} /> */}
->>>>>>> 78e475b453794cb1213734864d6e89c2e0116785
-         <Route path="/FORMULÁRIO" element={<Formulario />} /> 
+        <Route path="/REVOLUÇÃO_1930" element={<Revolucao1930 />} />
+        <Route path="/FORMULÁRIO" element={<Formulario />} />
         {/* <Route path="/AS_GATITAS" element={<AsGatitas />} /> */}
       </Routes>
     </Router>
