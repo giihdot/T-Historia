88477b744effe728a4cea252cca84c7c428da418
JuPiazzoli <jujubydomeneghetti@gmail.com>
2025-05-18 22:12:28 -0300

    Guerra do Contestado (CSS e JSX)

--- src/Pages/Guerra_Do_Contestado.jsx
+++ src/Pages/Guerra_Do_Contestado.jsx
@@ -3,6 +3,9 @@ import Header from "../Components/Header(Azul)";
 import Nav from "../Components/NavBar(Azul)";
 import Logo_Arma_Cinza from "../assets/LOGO - ARMA CINZA.png";
 import Avaliacao from "../Components/Avaliacao";
+import "./Guerra_Do_Contestado.css"
+import Ig1 from "../assets/F1(GCO).png";
+import Ig2 from "../assets/F2(GCO).png";
 
 function Contestado() {
   const [menuAberto, setMenuAberto] = useState(false);
@@ -44,26 +47,29 @@ function Contestado() {
       <div className="pagina-container">
         {menuAberto && <Nav />}
 
-        <main className="conteudo-principal">
+        <div className="conteudo-principal">
           <section>
-            <h2>GUERRA DO CONTESTADO</h2>
-            <h3>Resumo</h3>
-            <p>{resumoWiki}</p>
+            <h2 className="H1-GC">GUERRA DO CONTESTADO</h2>
+            <h3 className="H1-c">Resumo (Wikipédia)</h3>
+            <p className="ppCO">{resumoWiki}</p>
           </section>
 
-          <section>
-            <div>
-              <p>
-                A Guerra do Contestado foi um conflito armado que aconteceu
-                entre 1912 e 1916, na região que hoje abrange os estados do
-                Paraná e de Santa Catarina. O nome “Contestado” vem da disputa
-                territorial entre esses dois estados, mas o conflito envolveu
-                muito mais do que apenas fronteiras. Ele foi marcado por tensões
-                sociais, econômicas e religiosas. Tudo começou quando o governo
-                federal autorizou a construção da estrada de ferro São Paulo -
-                Rio Grande, realizada por uma empresa norte-americana. Para
-                isso, milhares de camponeses foram expulsos de suas terras sem
-                indenização. A empresa ainda recebeu grandes áreas como
+          <h2 className="H1-GC">Contexto</h2>
+          <p className="ppCO">
+            A Guerra do Contestado foi um conflito armado que aconteceu
+            entre 1912 e 1916, na região que hoje abrange os estados do
+            Paraná e de Santa Catarina. O nome “Contestado” vem da disputa
+            territorial entre esses dois estados, mas o conflito envolveu
+            muito mais do que apenas fronteiras. Ele foi marcado por tensões
+            sociais, econômicas e religiosas. Tudo começou quando o governo
+            federal autorizou a construção da estrada de ferro São Paulo -
+            Rio Grande, realizada por uma empresa norte-americana. Para
+            isso, milhares de camponeses foram expulsos de suas terras sem
+            indenização.
+          </p>
+          <div className="FR">
+            <div className="FC">
+              <p className="ppCO"> A empresa ainda recebeu grandes áreas como
                 pagamento, vendendo-as depois para fazendeiros ricos. Os
                 trabalhadores, abandonados e sem emprego, passaram a viver na
                 miséria. Esse cenário gerou revolta e levou ao surgimento de um
@@ -81,59 +87,64 @@ function Contestado() {
                 dos pobres por justiça e dignidade no Brasil.
               </p>
             </div>
-            <img
-              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTUP-MTQzLwjVd2fw3W2JSK7lXadOC_1aTdtw&s"
-              alt="Soldados na Guerra do Contestado"
-            />
-          </section>
+            <section className="FC">
+              <img id="Ig"
+                src={Ig1}
+                alt="Soldados na Guerra do Contestado"
+              />
+              <img id="Ig"
+                src={Ig2}
+                alt="Cavalaria do contestado"
+              />
+            </section>
+          </div>
 
-          <section>
-            <div>
-              <p>
-                Essas injustiças deram força a um movimento liderado por líderes
-                religiosos messiânicos, como o Monge José Maria. Ele era visto
-                como um profeta pelo povo, pregando uma vida baseada na fé, no
-                trabalho e na justiça social. Inspirados por suas palavras,
-                milhares de camponeses passaram a segui-lo, formando povoados
-                chamados de “comunidades santas”, onde tentavam viver com
-                igualdade, partilha e longe da opressão dos grandes fazendeiros
-                e do governo. Com o tempo, esses povoados passaram a ser vistos
-                como uma ameaça à ordem e aos interesses da elite. O governo,
-                então, enviou o Exército para acabar com o movimento. Foi o
-                início de uma guerra sangrenta, na qual os sertanejos resistiram
-                bravamente, mesmo sem armamento adequado. A repressão foi
-                violenta e muitos inocentes perderam a vida. A Guerra do
-                Contestado não foi apenas uma disputa por terra — foi a luta de
-                um povo abandonado por justiça, dignidade e fé..
-              </p>
-            </div>
-            <img
-              src="https://memoriapolitica.alesc.sc.gov.br/uploads/imagem/arquivo/A_Guerra_do_Contestado.jpeg"
-              alt="Monge José Maria"
-            />
-          </section>
 
-          <section>
-            <div className="video">
-              <iframe
-                width="560"
-                height="315"
-                src="https://www.youtube.com/embed/5n6kplSnks4?si=lDLpSt8i5ufO3EXY"
-                title="YouTube video player"
-                frameborder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                referrerpolicy="strict-origin-when-cross-origin"
-                allowfullscreen
-              ></iframe>
-            </div>
-            <img
-              src="https://www.uninter.com/noticias/wp-content/uploads/2019/08/noticias_contestado4.jpg"
-              alt="Mapa da Guerra do Contestado"
-            />
-            <Avaliacao />
-          </section>
-        </main>
-      </div>
+          <p className="ppCO">
+            Essas injustiças deram força a um movimento liderado por líderes
+            religiosos messiânicos, como o Monge José Maria. Ele era visto
+            como um profeta pelo povo, pregando uma vida baseada na fé, no
+            trabalho e na justiça social. Inspirados por suas palavras,
+            milhares de camponeses passaram a segui-lo, formando povoados
+            chamados de “comunidades santas”, onde tentavam viver com
+            igualdade, partilha e longe da opressão dos grandes fazendeiros
+            e do governo. Com o tempo, esses povoados passaram a ser vistos
+            como uma ameaça à ordem e aos interesses da elite. O governo,
+            então, enviou o Exército para acabar com o movimento. Foi o
+            início de uma guerra sangrenta, na qual os sertanejos resistiram
+            bravamente, mesmo sem armamento adequado. A repressão foi
+            violenta e muitos inocentes perderam a vida. A Guerra do
+            Contestado não foi apenas uma disputa por terra — foi a luta de
+            um povo abandonado por justiça, dignidade e fé..
+          </p>
+
+          <img
+            src="https://memoriapolitica.alesc.sc.gov.br/uploads/imagem/arquivo/A_Guerra_do_Contestado.jpeg"
+            alt="Monge José Maria"
+          />
+
+
+
+          <div className="video">
+            <iframe
+              width="560"
+              height="315"
+              src="https://www.youtube.com/embed/5n6kplSnks4?si=lDLpSt8i5ufO3EXY"
+              title="YouTube video player"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              referrerpolicy="strict-origin-when-cross-origin"
+              allowfullscreen
+            ></iframe>
+          </div>
+          <img
+            src="https://www.uninter.com/noticias/wp-content/uploads/2019/08/noticias_contestado4.jpg"
+            alt="Mapa da Guerra do Contestado"
+          />
+          <Avaliacao />
+
+        </div >
+      </div >
     </>
   );
 }
