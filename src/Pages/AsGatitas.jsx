9e95ad8baf031afef18f79d230195a684cc8f4e7
Kamilly Barra <kamillye033@gmail.com>
2025-05-21 14:00:35 -0300

    Alteração

--- src/Pages/AsGatitas.jsx
+++ src/Pages/AsGatitas.jsx
@@ -118,12 +118,8 @@ function AsGatitas() {
           <img src={LogoGatitas} alt="Logo Gatitas" className="logs" />
           <img src={CollabCinza} alt="Logo Arma Cinza" className="logs1" />
           <img src={LogoArmaCinza} alt="Logo Collab Cinza" className="logs2" /> 
-        </div>
-<<<<<<< HEAD
-        <Avaliacao/>
-=======
+        </div>       
         </section>
->>>>>>> 7ac9132214c094edcf1ebbc90d7934f18005c0c1
       </section>
       <Footer />
     </div>
