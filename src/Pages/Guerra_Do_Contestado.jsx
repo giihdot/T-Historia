9db30217134beb8366454ea2c084507105a49c70
Kamilly Barra <kamillye033@gmail.com>
2025-05-21 07:23:08 -0300

    Inclusão de informações em contestado

--- src/Pages/Guerra_Do_Contestado.jsx
+++ src/Pages/Guerra_Do_Contestado.jsx
@@ -3,7 +3,7 @@ import Header from "../Components/Header(Azul)";
 import Nav from "../Components/NavBar(Azul)";
 import Logo_Arma_Cinza from "../assets/LOGO - ARMA CINZA.png";
 import Avaliacao from "../Components/Avaliacao";
-import "./Guerra_Do_Contestado.css"
+import "./Guerra_Do_Contestado.css";
 import Ig1 from "../assets/F1(GCO).png";
 import Ig2 from "../assets/F2(GCO).png";
 import Ig3 from "../assets/F3(GCO).png";
@@ -54,31 +54,31 @@ function Contestado() {
             <h3 className="H1-c">Resumo (Wikipédia)</h3>
             <p className="ppCO">{resumoWiki}</p>
           </section>
-
           <h2 className="H1-GC">Contexto</h2>
           <p className="ppCO">
-            A Guerra do Contestado foi um conflito armado que aconteceu
-            entre 1912 e 1916, na região que hoje abrange os estados do
-            Paraná e de Santa Catarina. O nome “Contestado” vem da disputa
-            territorial entre esses dois estados, mas o conflito envolveu
-            muito mais do que apenas fronteiras. Ele foi marcado por tensões
-            sociais, econômicas e religiosas. Tudo começou quando o governo
-            federal autorizou a construção da estrada de ferro São Paulo -
-            Rio Grande, realizada por uma empresa norte-americana. Para
-            isso, milhares de camponeses foram expulsos de suas terras sem
-            indenização.
+            A Guerra do Contestado foi um conflito armado que aconteceu entre
+            1912 e 1916, na região que hoje abrange os estados do Paraná e de
+            Santa Catarina. O nome “Contestado” vem da disputa territorial entre
+            esses dois estados, mas o conflito envolveu muito mais do que apenas
+            fronteiras. Ele foi marcado por tensões sociais, econômicas e
+            religiosas. Tudo começou quando o governo federal autorizou a
+            construção da estrada de ferro São Paulo - Rio Grande, realizada por
+            uma empresa norte-americana. Para isso, milhares de camponeses foram
+            expulsos de suas terras sem indenização.
           </p>
           <div className="Row-F">
             <div className="FC">
-              <p className="ppCO"> A empresa ainda recebeu grandes áreas como
-                pagamento, vendendo-as depois para fazendeiros ricos. Os
-                trabalhadores, abandonados e sem emprego, passaram a viver na
-                miséria. Esse cenário gerou revolta e levou ao surgimento de um
-                movimento liderado por monges religiosos, como José Maria, que
-                pregavam justiça social, igualdade e uma vida cristã simples.
-                Esses líderes foram vistos como ameaças e o governo decidiu
-                reprimir o movimento com força militar. A guerra durou quatro
-                anos, com diversas batalhas entre o Exército e os sertanejos. Os
+              <p className="ppCO">
+                {" "}
+                A empresa ainda recebeu grandes áreas como pagamento,
+                vendendo-as depois para fazendeiros ricos. Os trabalhadores,
+                abandonados e sem emprego, passaram a viver na miséria. Esse
+                cenário gerou revolta e levou ao surgimento de um movimento
+                liderado por monges religiosos, como José Maria, que pregavam
+                justiça social, igualdade e uma vida cristã simples. Esses
+                líderes foram vistos como ameaças e o governo decidiu reprimir o
+                movimento com força militar. A guerra durou quatro anos, com
+                diversas batalhas entre o Exército e os sertanejos. Os
                 combatentes do Contestado, mesmo mal armados, resistiram
                 bravamente, motivados pela fé e pela esperança de uma vida
                 melhor. Ao final, milhares de pessoas morreram, e os
@@ -90,48 +90,46 @@ function Contestado() {
             </div>
             <section className="FC">
               <center>
-                <img id="Ig"
-                  src={Ig1}
-                  alt="Soldados na Guerra do Contestado"
-                />
-                <img id="Ig"
-                  src={Ig2}
-                  alt="Cavalaria do contestado"
-                />
+                <img id="Ig" src={Ig1} alt="Soldados na Guerra do Contestado" />
+                <img id="Ig" src={Ig2} alt="Cavalaria do contestado" />
               </center>
             </section>
           </div>
-
-
           <p className="ppCO">
             Essas injustiças deram força a um movimento liderado por líderes
-            religiosos messiânicos, como o Monge José Maria. Ele era visto
-            como um profeta pelo povo, pregando uma vida baseada na fé, no
-            trabalho e na justiça social. Inspirados por suas palavras,
-            milhares de camponeses passaram a segui-lo, formando povoados
-            chamados de “comunidades santas”, onde tentavam viver com
-            igualdade, partilha e longe da opressão dos grandes fazendeiros
-            e do governo. Com o tempo, esses povoados passaram a ser vistos
-            como uma ameaça à ordem e aos interesses da elite. O governo,
-            então, enviou o Exército para acabar com o movimento. Foi o
-            início de uma guerra sangrenta, na qual os sertanejos resistiram
-            bravamente, mesmo sem armamento adequado. A repressão foi
-            violenta e muitos inocentes perderam a vida. A Guerra do
-            Contestado não foi apenas uma disputa por terra — foi a luta de
+            religiosos messiânicos, como o Monge José Maria. Ele era visto como
+            um profeta pelo povo, pregando uma vida baseada na fé, no trabalho e
+            na justiça social. Inspirados por suas palavras, milhares de
+            camponeses passaram a segui-lo, formando povoados chamados de
+            “comunidades santas”, onde tentavam viver com igualdade, partilha e
+            longe da opressão dos grandes fazendeiros e do governo. Com o tempo,
+            esses povoados passaram a ser vistos como uma ameaça à ordem e aos
+            interesses da elite. O governo, então, enviou o Exército para acabar
+            com o movimento. Foi o início de uma guerra sangrenta, na qual os
+            sertanejos resistiram bravamente, mesmo sem armamento adequado. A
+            repressão foi violenta e muitos inocentes perderam a vida. A Guerra
+            do Contestado não foi apenas uma disputa por terra — foi a luta de
             um povo abandonado por justiça, dignidade e fé..
           </p>
-
           <center>
-            <img id="img3"
-              src={Ig3}
-              alt="Monge José Maria"
-            />
+            <img id="img3" src={Ig3} alt="Monge José Maria" />
           </center>
-
           <p className="ppCO">
-            PRECISO DE MAIS INFORMAÇÕES AQUI, KAMILLY! (APENAS PARA CONTINUAR O CSS, ESTOU ESCREVENDO E OCUPANDO COM PALAVRAS ESSA ÁREA!)
+          Foi um conflito ocorrido na fronteira dos estados do Paraná e Santa
+          Catarina, entre os anos de 1912 e 1916, por causa de disputas de
+          terras na região. A região era contestada, por isso o nome da guerra,
+          pelos dois estados, pois era rica em erva-mate e nela seria construída
+          uma estrada de ferro, ligando São Paulo ao Rio Grande do Sul. Milhares
+          de pessoas se deslocaram para a região, no intuito de trabalharem na
+          construção da ferrovia. Logo após a inauguração, os trabalhadores
+          ficaram desempregados, provocando uma crise social. Monges messiânicos
+          surgiram e formaram comunidades com os sertanejos empobrecidos, sendo
+          José Maria do Santo Agostinho o mais conhecido desses religiosos. A
+          formação dessas comunidades preocupou o governo federal, que enviou
+          várias tropas para a região, mas foram derrotadas pelos sertanejos.
+          Após uma luta sangrenta, o governo derrotou os sertanejos, e os dois
+          estados fizeram um acordo, estabelecendo os limites.
           </p>
-
           <div className="video">
             <iframe
               width="560"
@@ -145,9 +143,8 @@ function Contestado() {
             ></iframe>
           </div>
           <Avaliacao />
-
-        </div >
-      </div >
+        </div>
+      </div>
     </>
   );
 }
